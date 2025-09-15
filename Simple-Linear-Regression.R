f602833e794fd93d52f6a393861f0fb6f8b878f4
Ojochide14 <115094841+Ojochide14@users.noreply.github.com>
2025-09-15 00:30:57 -0400

    Update Simple-Linear-Regression.R

--- Simple-Linear-Regression.R
+++ Simple-Linear-Regression.R
@@ -1,5 +1,14 @@
-#Name: Ojochide Usman-Aguda Date:28/8/25 Purpose: Test simple linear regression
+#Author: Ojochide Usman-Aguda; Date: Aug 28, 2025; Purpose: Test linear regression
 
-#get some dummy dataset
+#Get some dummy dataset
+cars <- mtcars
 
-cars<- mtcars
+#Scatter plot on training data
+scatter.smooth(x=cars$mpg, y=cars$disp, main="SpeedVSdistance")
+
+#Calculate correlation value between mpg and disp
+cor(cars$mpg, cars$disp)
+
+#Split training and testing data for regression
+training_data <- cars[1:16,]
+test_data <- cars[17:32,]
